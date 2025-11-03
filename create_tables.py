28a0b6272490e11ca632dfddf4271328c095a1aa
JoaoPedro2001 <147657992+JoaoPedro2001@users.noreply.github.com>
2025-11-03 16:46:14 -0300

    Alterações na criação de tabelas
    Alterações no código de criação de tabelas no banco de dados, para que ele rode com o uso do Mysql

--- create_tables.py
+++ create_tables.py
@@ -1,16 +1,4 @@
 from database import engine, DBModel
+from models import *
 
-def main():
-    from models import (
-        Cliente, Atendente, Veiculo,
-        Categoria, Seguro, Reserva,
-        Pagamento, Manutencao, Multa,
-        reserva_seguro
-    )
-
-    with engine.connect() as conn:
-        DBModel.metadata.drop_all(conn)
-        DBModel.metadata.create_all(conn)
-
-if __name__ == '__main__':
-    main()
+DBModel.metadata.create_all(bind=engine)
